e24167a454e7c993a20e71edd1573145829d2d0e
Яна Мащенко <yanamashchenko@MacBook-Air-Dmitro.local>
2026-02-25 12:47:32 +0200

    Initialize npm project, install chalk and add color output in nodeBasics

---
+++ nodeBasics/index.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('Hello, I am learning Node.js!'));
+console.log(chalk.green('This text is green!'));
+console.log(chalk.red('This text is red!'));
