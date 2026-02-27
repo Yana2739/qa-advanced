e2cd6fc7668bff097a2d2ef35b7239d70b081c1b
Яна Мащенко <yanamashchenko@MacBook-Air-Dmitro.local>
2026-02-27 14:14:22 +0200

    Add js basics tasks 1-4

---
+++ jsBasics/task1.js
@@ -0,0 +1,13 @@
+// Primitive types
+
+const myString = "Hello World";
+const myNumber = 26;
+const myBoolean = true;
+const myNull = null;
+let myUndefined;
+
+console.log("String:", myString);
+console.log("Number:", myNumber);
+console.log("Boolean:", myBoolean);
+console.log("Null:", myNull);
+console.log("Undefined:", myUndefined);
---
+++ jsBasics/task2.js
@@ -0,0 +1,10 @@
+const firstName = "Anna";
+const secondName = "Yana";
+
+// Конкатенація
+const greetingConcat = "Hello, " + firstName + " and " + secondName + "!";
+console.log(greetingConcat);
+
+// Шаблонний рядок
+const greetingTemplate = `Hello, ${firstName} and ${secondName}!`;
+console.log(greetingTemplate);
